8fe4be175e4d35b7b89f823369899c24f0bd1e0e
LiangYu-Xidian <lyu@xidian.edu.cn>
2021-04-30 09:35:04 +0800

    Update word2vec.py

--- src/word2vec.py
+++ src/word2vec.py
@@ -17,4 +17,19 @@ for i in range(733):
 feature.close()
 fp1.close()
 
+fp2 = open('../walk_path/all_protein_walkpath.txt','r')
+walks = []
+for line in fp2:
+    walk = line.strip().split('\t')
+    walks.append(walk)
+model = Word2Vec(walks, size=400, window=4, sg=1, min_count=10 ,workers=10, iter=40, hs=0, negative=6)
+feature = open('../feature/protein_feature.txt','w')
+for i in range(1916):
+    if i == 0:
+        continue
+    for vec in model[str(i)]:
+        feature.write(str(vec)+' ')
+    feature.write('\n')
+feature.close()
+fp2.close()
 
